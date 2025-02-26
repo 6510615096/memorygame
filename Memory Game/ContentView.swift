8b90d73bf0b440a311605b3ef2e525de5ad2b8ab
Nutpupicha <6510615096@student.tu.ac.th>
2025-02-26 16:24:10 +0700

    first version

--- Memory Game/ContentView.swift
+++ Memory Game/ContentView.swift
@@ -1,24 +1,78 @@
 //
 //  ContentView.swift
-//  Memory Game
+//  Memorize
 //
-//  Created by นางสาวณัฐภูพิชา อรุณกรพสุรักษ์ on 26/2/2568 BE.
+//  Created by นางสาวณัฐภูพิชา อรุณกรพสุรักษ์ on 19/2/2568 BE.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🎃","💀","🕸️","😈","🎃","💀","🕸️","😈"]
+    @State var cardCount = 4
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ScrollView {
+                cards
+            }
+            cardCountAdjusters
         }
         .padding()
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 80))]) {
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+        }
+        .foregroundColor(.blue)
+    }
+    
+    var cardCountAdjusters : some View {
+        HStack {
+            cardCountAdjuster(by: -1, symbol: "rectangle.stack.fill.badge.minus")
+            Spacer()
+            cardCountAdjuster(by: 1, symbol: "rectangle.stack.fill.badge.plus")
+        }
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+                .imageScale(.large)
+                .font(.largeTitle)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct CardView: View {
+    @State var isFaceUp: Bool = true
+    var content: String
+    var body: some View {
+        ZStack {
+            var base = RoundedRectangle(cornerRadius: 12)
+//            var base = Ellipse()
+            Group {
+                base
+                    .fill(.white)
+                    .strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
 }
