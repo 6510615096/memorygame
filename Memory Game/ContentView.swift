2eb32a257b8c9604d306e082c4098206c6ee354c
Nutpupicha <6510615096@student.tu.ac.th>
2025-02-26 16:15:31 +0700

    Initial Commit

---
+++ Memory Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memory Game
+//
+//  Created by นางสาวณัฐภูพิชา อรุณกรพสุรักษ์ on 26/2/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memory Game/Memory_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_GameApp.swift
+//  Memory Game
+//
+//  Created by นางสาวณัฐภูพิชา อรุณกรพสุรักษ์ on 26/2/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct Memory_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
